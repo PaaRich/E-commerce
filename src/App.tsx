834cf6c98a6f62acb58e0ce25213932e6768244b
PaaRich <siehrichmond50@gmail.com>
2024-12-16 03:00:05 +0000

    installation

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,5 @@
 function App() {
- 
-  return (
-    <>
-      Hello world
-    </>
-  )
+  return <div className="text-red-600 bg-green-500">Hello world</div>;
 }
 
-export default App
+export default App;
