80f358144afb2072bf86705ed25761ea935ade00
PaaRich <siehrichmond50@gmail.com>
2025-02-04 23:26:52 +0000

    navbar

--- src/Components/NavBar/Navbar.tsx
+++ src/Components/NavBar/Navbar.tsx
@@ -3,8 +3,8 @@ import BottomNav from "./BottomNav";
 
 const Navbar = () => {
   return (
-    <div className="bg-[#000000e2] relative w-full lg:h-24">
-      <div className="lg:w-[90%] lg:mx-auto">
+    <div className="bg-[#000000e2] w-full h-24">
+      <div className="md:max-w-[90%] md:mx-auto">
         <TopNav />
         <BottomNav />
       </div>
